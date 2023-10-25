3e1b4073da9d589853a12411e80e10f3b48def46
umbertofaenza <umberto.faenza@gmail.com>
2023-10-25 16:29:05 +0200

    define Project relation to Type

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,4 +10,9 @@ class Project extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'description', 'link'];
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
